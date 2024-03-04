7e3e7e219e6c78b3885e422315cb3b878f6a9392
DuongDoi <duongdoi8400@gmail.com>
2024-03-04 21:27:07 +0700

    add

---
+++ Login/app/src/main/java/com/example/login/acticity_doctruyen.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class acticity_doctruyen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acticity_doctruyen);
+    }
+}

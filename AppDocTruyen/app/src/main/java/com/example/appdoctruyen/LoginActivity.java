310c0867615a7ae3570fc7df532cb6df6f4a5cb5
Dương Đối <149014682+DuongDoi@users.noreply.github.com>
2024-03-08 07:53:27 +0700

    Update LoginActivity.java

--- AppDocTruyen/app/src/main/java/com/example/appdoctruyen/LoginActivity.java
+++ AppDocTruyen/app/src/main/java/com/example/appdoctruyen/LoginActivity.java
@@ -18,40 +18,21 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Button loginButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
 
-        // Ánh xạ các phần tử UI
+        // Nút đăng nhập
+        Button loginButton;
         loginButton = findViewById(R.id.btn_login);
-        usernameEditText = findViewById(R.id.edittext_username);
-        passwordEditText = findViewById(R.id.edittext_repassword);
-
-        // Thiết lập lệnh cho nút đăng nhập
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // Lấy thông tin người dùng nhập vào
-                String username = usernameEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-
-                // Kiểm tra thông tin đăng nhập và xử lý logic tương ứng
-                if (isValidCredentials(username, password)) {
-                    // Đăng nhập thành công, chuyển sang màn hình chính hoặc màn hình tiếp theo
-                    // Ví dụ: startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                    Toast.makeText(LoginActivity.this,"Succesful.",Toast.LENGTH_SHORT).show();
-                } else {
-                    // Hiển thị thông báo lỗi hoặc xử lý khác
-                    Toast.makeText(LoginActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
-                }
-            }
-            // Phương thức kiểm tra tính hợp lệ của thông tin đăng nhập
-            private boolean isValidCredentials(String username, String password) {
-                // Kiểm tra logic đăng nhập ở đây, ví dụ: kiểm tra username và password có rỗng không
-                return !username.isEmpty() && !password.isEmpty();
+                Intent intent = new Intent(LoginActivity.this,activity_trangchu.class);
+                startActivity(intent);
             }
         });
 
+        //Nút đăng ký
         Button registerButton = findViewById(R.id.btn_reg);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
